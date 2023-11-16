028b79e3b3943e1e371477654f3e98f40a796c16
Alireza Abbasnia <cyneptic@gmail.com>
2023-11-16 18:55:19 +0330

    Fixed Tests but Have to think of a good test for styling functions

--- utils/config_loader_test.go
+++ utils/config_loader_test.go
@@ -11,7 +11,7 @@ import (
 
 func TestConfigLoader(t *testing.T) {
 	assert := assert.New(t)
-	tasks, err := utils.LoadConfig("sample_config.json")
+	tasks, _, err := utils.LoadConfig("sample_config.json")
 	require.NoError(t, err)
 
 	assert.Equal("walk", tasks[0].Name)
@@ -20,4 +20,12 @@ func TestConfigLoader(t *testing.T) {
 	assert.Equal("groceries", tasks[1].Name)
 	assert.Equal("buy groceries", tasks[1].Description)
 	assert.Equal(5*time.Minute, tasks[1].Timer.Timer())
+}
+
+func TestHourLoader(t *testing.T) {
+	assert := assert.New(t)
+	_, n, err := utils.LoadConfig("sample_config.json")
+	require.NoError(t, err)
+
+	assert.Greater(n, 0)
 }
